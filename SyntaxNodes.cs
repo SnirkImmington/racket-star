f917359c2347c5923cf1b0283701cdd5a95e01ea
Snirk Immington <snirk.immington@gmail.com>
2014-07-22 03:09:53 -0400

    Added syntax nodes file

---
+++ SyntaxNodes.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RacketInterpreter
+{
+    /// <summary>
+    /// Base class for all syntax nodes. <br/>
+    /// Each syntax node needs only to ultimately return a value.
+    /// </summary>
+    abstract class SyntaxNode
+    {
+        /// <summary>
+        /// Each syntax node should ultimately return a value.
+        /// </summary>
+        /// <returns></returns>
+        public abstract object getValue();
+    }
+
+    /// <summary>
+    /// SyntaxNode for chars or strings or ints/floats/doubles
+    /// which are known at compile time.
+    /// </summary>
+    class LiteralSyntaxNode : SyntaxNode
+    {
+        /// <summary>
+        /// The type of the variable - we're still working on types.
+        /// </summary>
+        public string Type;
+        /// <summary>
+        /// The value of the variable - known at compile time -
+        /// can be a string or char or number.
+        /// </summary>
+        public object Value;
+
+        // We will compile "strings", 'strings', chars ('c'), 
+        // numbers as ints, floats or doubles for C# interopability
+        public LiteralSyntaxNode(string type, object @value)
+        {
+            // value is a C# keyword, @value is a valid
+            // parameter name, no other significance
+            Type = type; Value = @value;
+        }
+
+        /// <summary>
+        /// Returns the literal value 
+        /// </summary>
+        /// <returns></returns>
+        public override object getValue()
+        {
+            return Value;
+        }
+    }
+}
