21d42ef173f451f15f8dcd0bba4303df951ee9b0
Timidger <APragmaticPlace@gmail.com>
2014-07-24 19:58:59 -0500

    Added C# naming -> Racket# naming conversion function. Does not yet support keyword mapping such as 'new'

--- Conversions.cs
+++ Conversions.cs
@@ -25,12 +25,11 @@ namespace RacketSharp
                     // Stop checking the thing
                     totalWordBuilder.Append(currentWordBuilder.ToString()).Append('-');
                     currentWordBuilder.Clear();
-                    currentWordBuilder.Append(char.ToLower(cSharpName[i]));
+
                 }
-                else // lowercase char
-                {
-                    currentWordBuilder.Append(char.ToLower(cSharpName[i]));
-                }
+                // Make the character lowercase
+                currentWordBuilder.Append(char.ToLower(cSharpName[i]));
+
             }
 
             if (currentWordBuilder.Length > 0)
@@ -40,6 +39,52 @@ namespace RacketSharp
 
             if (text.EndsWith("-"))
                 return text.Substring(0, text.Length - 2);
+            return text;
+        }
+
+        public static string getCSharpName(string racketSharpName, string variableType) //make that an enum
+        {
+            var currentWordBuilder = new StringBuilder();
+            var totalWordBuilder = new StringBuilder();
+            // Check if need to capitalize
+            bool isNewWord;
+            // Naming conventions are different for functions and variables in C#
+            if (variableType.equals("function"))
+                isNewWord = true;
+            else if (variableType.equals("variable"))
+                isNewWord = false;
+            else:
+                isNewWord = true; // An enum should solve this particular possibility (error)
+
+            for (int i = 0; i < racketSharpName.Length; i++)
+            {
+                if (char.IsLower(racketSharpName[i]) && isNewWord)
+                {
+                    // Stop checking the string
+                    currentWordBuilder.Append(char.ToLower(racketSharpName[i]));
+                    isNewWord = false;
+                }
+
+                else if (racketSharpName.Equals('_')
+                {
+                    // Makes the next character uppercase
+                    isNewWord = true;
+                }
+
+                else if (racketSharpName.equals('-')
+                {
+                    // Makes the character a period because we are going to access an attribute of the variable
+                    currentWordBuilder.Append('.');
+                    isNewWord = true;
+                }
+                else
+                    currentWordBuilder.Append(racketSharpName[i]);
+            }
+
+            if (currentWordBuilder.Length > 0)
+                totalWordBuilder.Append(currentWordBuilder.ToString());
+
+            var text = totalWordBuilder.ToString();
             return text;
         }
     }
