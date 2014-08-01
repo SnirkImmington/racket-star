dd14b473f86cf629ce5d990d95f7aa099c5b1563
Snirk Immington <snirk.immington@gmail.com>
2014-08-01 23:32:33 +1000

    Added testing class

---
+++ racket-sharp/Testing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace racket_sharp
+{
+    /// <summary>
+    /// Syntax node with specific value for testing.
+    /// </summary>
+    class ValueTestingSyntaxNode : SyntaxNode
+    {
+        public object value;
+        public override object GetValue()
+        {
+            return value;
+        }
+        public ValueTestingSyntaxNode(object theValue)
+        {
+            value = theValue;
+        }
+    }
+}
