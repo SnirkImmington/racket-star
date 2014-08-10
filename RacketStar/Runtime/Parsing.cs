2716c31e1de99447566885a309c5498e293ab4fa
Snirk Immington <snirk.immington@gmail.com>
2014-08-11 01:30:36 +1000

    Adding expression finding method

--- RacketStar/Runtime/Parsing.cs
+++ RacketStar/Runtime/Parsing.cs
@@ -11,7 +11,7 @@ namespace RacketStar.Runtime
         public static CompileUnit ParseLine(string text)
         {
             var nodes = new List<SyntaxNode>();
-            StringBuilder current = new StringBuilder();
+            var current = new StringBuilder();
             for (int i = 0; i < text.Length; i++)
             {
                 if (text[i] == ')')
@@ -19,14 +19,51 @@ namespace RacketStar.Runtime
                     for (int tillOpen = i; tillOpen >= 0; tillOpen++)
                     {
                         if (text[tillOpen] == '(')
+                        {
+                            var node = ParseExpression(text.Substring(tillOpen, i - tillOpen + 1));
+                        }
                     }
                 }
             }
         }
 
-        public static SyntaxNode ParseExpression(string expression)
+        /// <summary>
+        /// Creates a SyntaxNode from the given text.
+        /// </summary>
+        /// <param name="expression">Text to parse, should not contain start/finish parenthesis.</param>
+        /// <returns>A parsed SyntaxNode.</returns>
+        private static SyntaxNode ParseExpression(string expression)
         {
+            // Split the node into arguments.
+            var current = new StringBuilder();
+            var args = new List<string>();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == ' ')
+                {
 
+                }
+                else if (expression[i] == '(')
+
+            }
+        }
+
+        private static string FindExpression(string text, int start)
+        {
+            var count = 1;
+            var builder = new StringBuilder();
+            for (int i = start; i < text.Length; i++)
+            {
+                switch (text[i])
+                {
+                    case '(': count++; break;
+                    case ')': count--; break;
+                    default: builder.Append(text[i]); break;
+                }
+
+                if (count == 0) return builder.ToString();
+            }
+            // Throw exception?
         }
     }
 
