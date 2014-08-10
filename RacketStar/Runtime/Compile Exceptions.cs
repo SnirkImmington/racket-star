1e92955bf656f2e009d39ff31f98214cc08d7761
Snirk Immington <snirk.immington@gmail.com>
2014-08-11 01:37:05 +1000

    Added missing parenthesis exception

---
+++ RacketStar/Runtime/Compile Exceptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RacketStar.Runtime
+{
+    abstract class CompilationException : Exception
+    {
+        public int StartIndex;
+        public int EndIndex;
+
+        public CompilationException(int start, int finish, string message) : base(message)
+        { 
+            StartIndex = start; EndIndex = finish;
+        }
+    }
+
+    class MissingParenthesisException : CompilationException
+    {
+        public MissingParenthesisException(int start, int finish, string message)
+            : base(start, finish, message) { }
+    }
+}
