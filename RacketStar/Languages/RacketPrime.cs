a832425a93d89e4d8ebf47d71c91cfafbd6300ce
Snirk Immington <snirk.immington@gmail.com>
2014-08-17 01:33:07 +1000

    Stuffier stuff

---
+++ RacketStar/Languages/RacketPrime.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RacketStar.Languages
+{
+    class RacketPrime : Language
+    {
+        public static bool __boolean(object input)
+        {
+            return input is bool;
+        }
+
+        public static string GetTypeMatch(string typeName)
+        {
+            switch (typeName)
+            {
+                case "boolean": return "bool";
+            }
+            return "";
+        }
+
+        public override string GetMethodReplacement(string method)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RacketStar/Languages/RacketSharp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RacketStar.Languages
+{
+    class RacketSharp : Language
+    {
+        public override string GetMethodReplacement(string input)
+        {
+            switch (input)
+            {
+                // Support for x++/x--: usage in for loop style syntax
+                case "++": return "op_Increment";
+                case "--": return "op_Decrement";
+
+                case ">>": return "op_LeftShift";
+                case "<<": return "op_RightShift";
+
+                case "==": return "op_Equal";
+                case "set": return "op_Assign";
+
+                default: return input;
+            }
+        }
+    }
+}
