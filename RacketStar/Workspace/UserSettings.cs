a816fca97576bc7f2dec34bc039247315c6816b6
Snirk Immington <snirk.immington@gmail.com>
2014-08-17 01:32:39 +1000

    More stuff:

---
+++ RacketStar/Workspace/UserSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace RacketStar.Workspace
+{
+    class UserSettings
+    {
+        public Dictionary<string, object> Values = new Dictionary<string, object>();
+
+        public void SetDefaults()
+        {
+
+        }
+
+        public void ReadFromFile()
+        {
+        }
+
+
+    }
+}
